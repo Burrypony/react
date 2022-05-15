4777e05ba15f91188dd5c37c25623ae3e3f91985
Burrypony <kiril.chernysh@gmail.com>
2022-05-16 01:33:25 +0300

    last version of project

--- hooks/widgets/src/components/Dropdown.js
+++ hooks/widgets/src/components/Dropdown.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Dropdown = ({options, selected, onSelectedChange}) =>{
+    const [open, setOpen] = React.useState(false)
+
+    useEffect(()=>{
+        document.body.addEventListener('click',() =>{
+            setOpen(!open)
+        })
+    },[])
+
     const renderedOptions = options.map((option) =>{
+
+        if(option.value === selected.value){
+            return null;
+        }
+
         return(
             <div key={option.value} className="item" onClick={()=>onSelectedChange(option)}>
                 {option.label}
@@ -14,12 +27,12 @@ const Dropdown = ({options, selected, onSelectedChange}) =>{
         <div className="ui form">
             <div className="field">
                 <label className="lable">Select a Color</label>
-                <div className="ui selection dropdown visisble active">
+                <div onClick={()=> setOpen(!open)} className={`ui selection dropdown ${open?'visisble active':''}`}>
                     <i className="dropdown icon"></i>
                     <div className="text">
                         {selected.label}
                     </div>
-                    <div className="menu visible transition">
+                    <div className={`menu ${open?'visible transition':''}`}>
                          {renderedOptions}
                     </div>
                 </div>
